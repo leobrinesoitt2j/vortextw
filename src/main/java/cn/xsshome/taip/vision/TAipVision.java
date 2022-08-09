b6fa80089fefbb47b4856a9f918788ce2c646fba
leobrinesoitt2j <liulushang31328596@163.com>
2022-08-09 10:05:46 +0800

    :sparkles:暴恐鉴黄接口支持图片URL识别

--- src/main/java/cn/xsshome/taip/vision/TAipVision.java
+++ src/main/java/cn/xsshome/taip/vision/TAipVision.java
@@ -37,6 +37,29 @@ public class TAipVision extends BaseClient{
         result = requestServer(request);
         return result;
     }
+    /**
+     * 暴恐图片识别 	
+     * 识别一个图像是否为暴恐图像。 	
+     * @param image_url - 待识别图片url
+     * @return String
+	 * @throws Exception 
+     */
+    public String imageTerrorismByURL(String image_url) throws Exception{
+    	String result ="";
+        TAipRequest request = new TAipRequest();
+		String time_stamp = System.currentTimeMillis()/1000+"";
+		request.addBody("app_id", app_id);
+		request.addBody("time_stamp", time_stamp);
+		request.addBody("nonce_str", RandomNonceStrUtil.getRandomString());
+        request.addBody("image_url", image_url);
+        String sign = TencentAISignSort.getSignature(request.getBody(),app_key);
+		request.addBody("sign",sign);
+        request.setUri(VisionConsts.IMAGE_TERRORISM);
+        request.setBodyFormat(TAipEBodyFormat.FORM_KV);
+        result = requestServer(request);
+        return result;
+    }
+    
 	 /**
      * 暴恐图片识别 	
      * 识别一个图像是否为暴恐图像。 	
@@ -70,7 +93,30 @@ public class TAipVision extends BaseClient{
         request.setBodyFormat(TAipEBodyFormat.FORM_KV);
         result = requestServer(request);
         return result;
+    } 
+    /**
+     * 智能鉴黄 	
+     * 识别一个图像是否为色情图像。 	
+     * @param image_url - 待识别图片url
+     * @return String
+	 * @throws Exception 
+     */
+    public String visionPornByURL(String image_url) throws Exception{
+    	String result ="";
+        TAipRequest request = new TAipRequest();
+		String time_stamp = System.currentTimeMillis()/1000+"";
+		request.addBody("app_id", app_id);
+		request.addBody("time_stamp", time_stamp);
+		request.addBody("nonce_str", RandomNonceStrUtil.getRandomString());
+        request.addBody("image_url", image_url);
+        String sign = TencentAISignSort.getSignature(request.getBody(),app_key);
+		request.addBody("sign",sign);
+        request.setUri(VisionConsts.VISION_PORN);
+        request.setBodyFormat(TAipEBodyFormat.FORM_KV);
+        result = requestServer(request);
+        return result;
     }
+    
 	 /**
      * 智能鉴黄 	
      * 识别一个图像是否为色情图像
